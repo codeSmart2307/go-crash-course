4e88be58df6e76866fb37148e0f1e67f61f77215
Raneesh Gomez <raneesh2307@gmail.com>
2019-06-01 16:42:09 +0530

    add more modules

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -7,10 +7,14 @@ import (
 
 // Define Person struct
 type Person struct {
-	firstName string
-	lastName string
-	city string
-	gender string
+	//firstName string
+	//lastName string
+	//city string
+	//gender string
+	//age int
+
+	// This is a shorter way to define a struct
+	firstName, lastName, city, gender string
 	age int
 }
 
@@ -31,12 +35,21 @@ func (p *Person) hasBirthday() {
 	p.age++
 }
 
+// getMarried method (Pointer receiver)
+func (p *Person) getMarried(spouseLastName string) {
+	if p.gender == "M" {
+		return;
+	} else {
+		p.lastName = spouseLastName
+	}
+}
+
 func main() {
 	// Init person using struct
 	person1 := Person{firstName: "Raneesh", lastName: "Gomez", city: "Ragama", gender: "M", age: 22}
 
 	// Alternative way to initialize a person
-	person2 := Person{"Dilum", "De Silva","Piliyandala", "M", 24}
+	person2 := Person{"Ashley", "Halpe","Deniyaya", "F", 24}
 
 	fmt.Println(person1)
 	fmt.Println(person1.firstName)
@@ -50,4 +63,7 @@ func main() {
 	fmt.Println(person1.greet())
 	person1.hasBirthday()
 	fmt.Println(person1.greet())
+
+	person2.getMarried(person1.lastName)
+	fmt.Println(person2.greet())
 }
